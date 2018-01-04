79df12796958635e19a2a0a42d11dee2bdb29111
kapj17n-13 <kapj17n-13@sr.dmcs.pl>
2018-01-04 22:15:13 +0100

    obliczanie wartosci odzywczych dla przepisow, proste widoki listy przepisow i przepisu

--- src/main/java/com/projekt/zespolowy/models/Recipe.java
+++ src/main/java/com/projekt/zespolowy/models/Recipe.java
@@ -8,11 +8,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.JoinColumn;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "recipe")
@@ -24,6 +25,8 @@ public class Recipe {
 	private long id;
 	private String name;
 	private String description;
+	@Lob
+	@Column(length=5000)
 	private String preparation;
 	@Lob
 	private Blob photo;
@@ -38,6 +41,80 @@ public class Recipe {
 	        inverseJoinColumns = @JoinColumn(name = "category_id")
 	)
 	private Set<Category> category;
+	
+	@Transient
+	private float totalCalories;
+	
+	@Transient
+	private float totalSugar;
+	
+	@Transient
+	private float totalProtein;
+	
+	@Transient
+	private float totalFat;
+	
+	@Transient
+	private float totalIndexg;
+	
+	
+	public float getTotalCalories() {
+		float totalcalories = 0;
+		for(RecipeIngredient recipeIngredient: getRecipeIngredient()) {
+			totalcalories += recipeIngredient.getIngredient().getCalories() * recipeIngredient.getAmount()/100;
+		}
+		return totalcalories;
+	}
+	public void setTotalCalories(float totalCalories) {
+		this.totalCalories = totalCalories;
+	}
+		
+	public float getTotalSugar() {
+		float totalsugar = 0;
+		for(RecipeIngredient recipeIngredient: getRecipeIngredient()) {
+			totalsugar += recipeIngredient.getIngredient().getSugar() * recipeIngredient.getAmount()/100;
+		}
+		return totalsugar;
+	}
+	public void setTotalSugar(float totalSugar) {
+		this.totalSugar = totalSugar;
+	}
+		
+	public float getTotalProtein() {
+		float totalprotein = 0;
+		for(RecipeIngredient recipeIngredient: getRecipeIngredient()) {
+			totalprotein += recipeIngredient.getIngredient().getProtein() * recipeIngredient.getAmount()/100;
+		}
+		return totalprotein;
+	}
+	public void setTotalProtein(float totalProtein) {
+		this.totalProtein = totalProtein;
+	}
+	
+	public float getTotalFat() {
+		float totalfat = 0;
+		for(RecipeIngredient recipeIngredient: getRecipeIngredient()) {
+			totalfat += recipeIngredient.getIngredient().getFat() * recipeIngredient.getAmount()/100;
+		}
+		return totalfat;
+	}
+	public void setTotalFat(float totalFat) {
+		this.totalFat = totalFat;
+	}
+	
+	public float getTotalIndexg() {
+		float totalindexg =0;
+		float totalsugar = getTotalSugar();
+		for(RecipeIngredient recipeIngredient: getRecipeIngredient()) {
+			float ingredientSugar = (float)recipeIngredient.getIngredient().getSugar() * recipeIngredient.getAmount()/100;
+			totalindexg += (ingredientSugar/totalsugar)*recipeIngredient.getIngredient().getIndexg();
+		}
+		return totalindexg;
+	}
+	
+	public void setTotalIndexg(float totalIndexg) {
+		this.totalIndexg = totalIndexg;
+	}
 	
 	public Set<Category> getCategory() {
 		return category;
